641e1b55d66cbf3113d8a91c6d31135f36a87162
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-03 02:04:38 +0300

    Update error handler and assets init

--- includes/handler.h
+++ includes/handler.h
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 01:35:22 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/03 01:41:25 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/03 02:04:31 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ typedef struct s_cub	t_cub;
 typedef struct s_assets t_assets;
 
 void	handler_loop(void* data);
+void	handler_close(void* data);
 
 #endif
