d7b8e505c0d406e6698984b00ec734a99dc6dc30
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-05 10:48:43 +0300

    Update ray casting and render

--- Srcs/Execution/Handler/Render/render_player.c
+++ Srcs/Execution/Handler/Render/render_player.c
@@ -6,8 +6,17 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 21:40:52 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/04 21:40:57 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/05 10:42:26 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	render_player(t_cub *c)
+{
+	t_sprite *const	sprite = c->assets.sprite;
+	mlx_image_t	*player	= c->assets.player;
+
+	render_px_cpy(c->assets.player, sprite->frame[sprite->frame_id],
+				player->width, player->height);
+}
