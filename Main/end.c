d00208c6e75327f851cab40f0e94f8dc163f2fab
Siuol7 <nguyencaotan03072000@gmail.com>
2025-06-09 14:12:41 +0300

    Update completed leak handled

--- Main/end.c
+++ Main/end.c
@@ -6,7 +6,7 @@
 /*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 02:32:41 by caonguye          #+#    #+#             */
-/*   Updated: 2025/06/09 14:02:45 by caonguye         ###   ########.fr       */
+/*   Updated: 2025/06/09 14:12:38 by caonguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	cub3d_ends(t_cub *c, char *str, int exitcode)
 	if (!c)
 		return ;
 	if (c->mlx != NULL)
-			mlx_close_window(c->mlx);
+		mlx_close_window(c->mlx);
 	clean_assets(c, &c->assets);
 	clean_map(&c->map);
 	if (exitcode)
 		ft_printf_fd(2, "%s\n", str);
 	if (c->mlx)
-			mlx_terminate(c->mlx);
+		mlx_terminate(c->mlx);
 	exit(exitcode);
 }
